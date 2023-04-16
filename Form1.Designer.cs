1528b29e082f6b12ce2865816f2276ff88b335ce
vitrolha <110424603+vitrolha@users.noreply.github.com>
2023-04-16 12:42:57 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AgendaSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_AddTarefa = new System.Windows.Forms.TextBox();
+            this.btn_Adicionar = new System.Windows.Forms.Button();
+            this.listaTarefas = new System.Windows.Forms.ListView();
+            this.btn_Excluir = new System.Windows.Forms.Button();
+            this.btn_Atualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox_AddTarefa
+            // 
+            this.txtBox_AddTarefa.Location = new System.Drawing.Point(12, 12);
+            this.txtBox_AddTarefa.Multiline = true;
+            this.txtBox_AddTarefa.Name = "txtBox_AddTarefa";
+            this.txtBox_AddTarefa.Size = new System.Drawing.Size(776, 52);
+            this.txtBox_AddTarefa.TabIndex = 0;
+            // 
+            // btn_Adicionar
+            // 
+            this.btn_Adicionar.Location = new System.Drawing.Point(12, 83);
+            this.btn_Adicionar.Name = "btn_Adicionar";
+            this.btn_Adicionar.Size = new System.Drawing.Size(581, 36);
+            this.btn_Adicionar.TabIndex = 1;
+            this.btn_Adicionar.Text = "Adicionar";
+            this.btn_Adicionar.UseVisualStyleBackColor = true;
+            this.btn_Adicionar.Click += new System.EventHandler(this.btn_Adicionar_Click);
+            // 
+            // listaTarefas
+            // 
+            this.listaTarefas.HideSelection = false;
+            this.listaTarefas.Location = new System.Drawing.Point(12, 139);
+            this.listaTarefas.Name = "listaTarefas";
+            this.listaTarefas.Size = new System.Drawing.Size(776, 299);
+            this.listaTarefas.TabIndex = 2;
+            this.listaTarefas.UseCompatibleStateImageBehavior = false;
+            // 
+            // btn_Excluir
+            // 
+            this.btn_Excluir.Location = new System.Drawing.Point(599, 83);
+            this.btn_Excluir.Name = "btn_Excluir";
+            this.btn_Excluir.Size = new System.Drawing.Size(94, 36);
+            this.btn_Excluir.TabIndex = 3;
+            this.btn_Excluir.Text = "Excluir";
+            this.btn_Excluir.UseVisualStyleBackColor = true;
+            this.btn_Excluir.Click += new System.EventHandler(this.btn_Excluir_Click);
+            // 
+            // btn_Atualizar
+            // 
+            this.btn_Atualizar.Location = new System.Drawing.Point(699, 83);
+            this.btn_Atualizar.Name = "btn_Atualizar";
+            this.btn_Atualizar.Size = new System.Drawing.Size(89, 36);
+            this.btn_Atualizar.TabIndex = 4;
+            this.btn_Atualizar.Text = "Atualizar";
+            this.btn_Atualizar.UseVisualStyleBackColor = true;
+            this.btn_Atualizar.Click += new System.EventHandler(this.btn_Atualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Atualizar);
+            this.Controls.Add(this.btn_Excluir);
+            this.Controls.Add(this.listaTarefas);
+            this.Controls.Add(this.btn_Adicionar);
+            this.Controls.Add(this.txtBox_AddTarefa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_AddTarefa;
+        private System.Windows.Forms.Button btn_Adicionar;
+        private System.Windows.Forms.ListView listaTarefas;
+        private System.Windows.Forms.Button btn_Excluir;
+        private System.Windows.Forms.Button btn_Atualizar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+namespace AgendaSQL
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conexao;
+        private string data_source = "datasource=localhost;username=root;password=;database=agenda";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listaTarefas.View = View.Details;
+            listaTarefas.LabelEdit = true;
+            listaTarefas.AllowColumnReorder = true;
+            listaTarefas.FullRowSelect = true;
+            listaTarefas.GridLines = true;
+
+            listaTarefas.Columns.Add("ID",30,HorizontalAlignment.Left);
+            listaTarefas.Columns.Add("Conteudo", 500, HorizontalAlignment.Left);
+
+            exibirTarefas();
+        }
+
+        //Botão adicionar tarefa
+        private void btn_Adicionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtBox_AddTarefa.Text != "")
+                {
+                    adicionarTarefas();
+                }
+                else
+                {
+                    MessageBox.Show("Digite algo");
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                exibirTarefas();
+            }
+        }
+
+        //Botão excluir tarefa
+        private void btn_Excluir_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmar = MessageBox.Show("Deseja realmente atualizar tarefa?", "Atualizar", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+
+            if (confirmar == DialogResult.Yes)
+            try
+            {
+                excluirTarefas();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //MessageBox.Show("Tarefa Excluida");
+                exibirTarefas();
+            }
+            
+        }
+
+        //Botão Atualizar Tarefa
+        private void btn_Atualizar_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmar = MessageBox.Show("Deseja realmente atualizar tarefa?", "Atualizar", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation,MessageBoxDefaultButton.Button2);
+
+            if (confirmar == DialogResult.Yes && txtBox_AddTarefa.Text != "")
+            {
+                try
+                {
+                    atualizarTarefas();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    //MessageBox.Show("Tarefa Atualizada");
+                    exibirTarefas();
+                }
+            }
+            else if(txtBox_AddTarefa.Text == "")
+            {
+                MessageBox.Show("Não é possivel atualizar para texto em branco");
+            }
+        }
+
+        public void adicionarTarefas()
+        {
+            conexao = new MySqlConnection(data_source);
+
+            string sql = $"insert into tarefas (conteudo) values ('{txtBox_AddTarefa.Text}') ";
+
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            conexao.Open();
+
+            comando.ExecuteReader();
+
+            MessageBox.Show("Tarefa Adicionada");
+
+            conexao.Close();
+        }
+
+        public void exibirTarefas()
+        {
+            conexao = new MySqlConnection(data_source);
+
+            string sql = "select * from tarefas";
+
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            conexao.Open();
+
+            MySqlDataReader reader = comando.ExecuteReader();
+
+            listaTarefas.Items.Clear();
+
+            while (reader.Read())
+            {
+                string[] row =
+                {
+                        reader.GetString(0),
+                        reader.GetString(1)
+                    };
+
+                var linha_listview = new ListViewItem(row);
+
+                listaTarefas.Items.Add(linha_listview);
+            }
+
+            conexao.Close();
+        }
+
+        public void excluirTarefas()
+        {
+            conexao = new MySqlConnection(data_source);
+
+            if (listaTarefas.SelectedItems.Count > 0)
+            {
+                // Obtém a linha selecionada
+                ListViewItem linhaSelecionada = listaTarefas.SelectedItems[0];
+
+                // Obtém o valor da coluna "Id"
+                int valorId = Convert.ToInt32(linhaSelecionada.SubItems[0].Text);
+
+                // Usa o valor como necessário
+                //MessageBox.Show($"O valor da coluna 'Id' é {valorId}");
+
+                string sql = $"delete from tarefas where tarefa_id = {valorId};";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                conexao.Open();
+
+                comando.ExecuteReader();
+
+                //MessageBox.Show("Tarefa Excluida");
+
+                conexao.Close();
+
+            }
+
+
+            
+        }
+
+        public void atualizarTarefas()
+        {
+            conexao = new MySqlConnection(data_source);
+
+            if (listaTarefas.SelectedItems.Count > 0)
+            {
+                // Obtém a linha selecionada
+                ListViewItem linhaSelecionada = listaTarefas.SelectedItems[0];
+
+                // Obtém o valor da coluna "Id"
+                int valorId = Convert.ToInt32(linhaSelecionada.SubItems[0].Text);
+
+                string sql = $"update tarefas set conteudo = '{txtBox_AddTarefa.Text}' where tarefa_id = {valorId}";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                conexao.Open();
+
+                comando.ExecuteReader();
+
+                conexao.Close();
+            }
+
+        }
+
+    }
+}
